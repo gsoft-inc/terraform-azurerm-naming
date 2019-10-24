636dde23cfe4f22da0176ddd8327d1a5dda134fb
Yohan Belval <yohan.belval@gsoft.com>
2019-10-24 14:47:20 -0400

    Added regex to storage account

--- storage/storage_account/outputs.tf
+++ storage/storage_account/outputs.tf
@@ -1,4 +1,4 @@
 output "result" {
   description = "The generated storage account name."
-  value       = lower(module.storage_account.result)
+  value       = regex("^[a-zA-Z0-9]*$", lower(module.storage_account.result))
 }
